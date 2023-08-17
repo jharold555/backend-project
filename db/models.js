1032f9b9441ef75af8047496946c9cd26196f817
Jasmine <jasmine.mjharold@gmail.com>
2023-08-17 18:22:18 +0100

    updated task 11

--- db/models.js
+++ db/models.js
@@ -1,7 +1,7 @@
 const db = require("./connection.js");
-const {checkExists, patchVotes, deleteItem}= require("./utils.js");
+const { checkExists, patchVotes, deleteItem } = require("./utils.js");
 const fsPromises = require("fs").promises;
-const format = require('pg-format')
+const format = require("pg-format");
 
 const getApiData = async () => {
   const data = await fsPromises.readFile(
@@ -27,30 +27,40 @@ const getArticlesData = async (
   topic
 ) => {
   const validOrder = ["asc", "desc"];
-  
+
   if (!validOrder.includes(order)) {
     return Promise.reject({ status: 400, msg: "400 Bad Request" });
   }
   try {
     let column = sort_by;
-    let queryStr = format(
-      `SELECT articles.*, 
+    let queryStr1 = format(
+      `SELECT 
+      articles.article_id,
+      articles.title,
+      articles.topic,
+      articles.author,
+      articles.created_at,
+      articles.votes,
+      articles.article_img_url, 
     COUNT(comment_id) 
     AS comment_count FROM articles 
-    LEFT JOIN comments ON comments.article_id = articles.article_id 
-    GROUP BY articles.article_id
+    LEFT JOIN comments ON comments.article_id = articles.article_id`
+    );
+
+    let queryStr2 = format(
+      ` GROUP BY articles.article_id
     ORDER BY %I`,
       column
     );
-    order === "asc" ? (queryStr += ` ASC`) : (queryStr += ` DESC`);
-
-    const articlesComments = await db.query(queryStr);
-    const articlesArray = articlesComments.rows;
-    articlesArray.forEach((article) => delete article.body);
+    order === "asc" ? (queryStr2 += ` ASC`) : (queryStr2 += ` DESC`);
+    
     if (topic) {
-      await checkExists("articles", "topic", topic);
-      return articlesArray.filter((article) => article.topic === topic);
-    } else return articlesArray;
+      const topicStr = format(` WHERE topic = $1 `);
+      const articles = await db.query(queryStr1 + topicStr + queryStr2, [topic]);
+      return articles.rows;
+    }
+    const articles = await db.query(queryStr1 + queryStr2);
+    return articles.rows;
   } catch (error) {
     throw error;
   }
@@ -68,35 +78,34 @@ getCommentsData = async (id) => {
       msg: `404 comments Not Found`,
     });
   }
-  return commentsArray
+  return commentsArray;
 };
 const insertComment = async (body, id) => {
-  try{
-  const user = body.username;
-  await checkExists("articles", "article_id", id);
-  await checkExists("users", "username", user);
-  const comment = {
-    article_id: id,
-    author: user,
-    votes: 0,
-    body: body.body,
-    created_at: new Date(),
-  };
-  const vals = Object.values(comment);
-  const commentQ =  await db.query(
-    `INSERT INTO comments (article_id, author, votes, body, created_at) 
+  try {
+    const user = body.username;
+    await checkExists("articles", "article_id", id);
+    await checkExists("users", "username", user);
+    const comment = {
+      article_id: id,
+      author: user,
+      votes: 0,
+      body: body.body,
+      created_at: new Date(),
+    };
+    const vals = Object.values(comment);
+    const commentQ = await db.query(
+      `INSERT INTO comments (article_id, author, votes, body, created_at) 
   VALUES ($1, $2, $3, $4, $5)
   RETURNING *;`,
-    vals
-  );
-  return commentQ.rows
-}
-catch(error){
-  if(error.msg.includes('Not Found')){
-    return Promise.reject({status: 400, msg: '400 Bad Request'})
+      vals
+    );
+    return commentQ.rows;
+  } catch (error) {
+    if (error.msg.includes("Not Found")) {
+      return Promise.reject({ status: 400, msg: "400 Bad Request" });
+    }
+    throw error;
   }
-  throw error
-}
 };
 const changeArticleVotes = async (obj, id) => {
   await checkExists("articles", "article_id", id);
@@ -112,4 +121,14 @@ const getUsersData = async () => {
   const users = await db.query("SELECT * FROM users");
   return users.rows;
 };
-module.exports = { getTopicsData, getApiData, getArticleData, getArticlesData, getCommentsData, insertComment, changeArticleVotes, commentDelete, getUsersData };
+module.exports = {
+  getTopicsData,
+  getApiData,
+  getArticleData,
+  getArticlesData,
+  getCommentsData,
+  insertComment,
+  changeArticleVotes,
+  commentDelete,
+  getUsersData,
+};
